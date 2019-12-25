dc986a77f4f5a182f1d2fdd438d09ee96be4c396
Jose Fonseca <jfonseca@zefonseca.com>
2019-12-25 20:53:54 -0300

    Starting work on toolbox/db/blocks_updater.cpp

---
+++ toolbox/db/blocks_updater.cpp
@@ -0,0 +1,17 @@
+/*
+ *
+ * Crypto.BI Toolbox
+ * https://Crypto.BI/
+ *
+ * Author: José Fonseca (https://zefonseca.com/)
+ *
+ * Distributed under the MIT software license, see the accompanying
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.
+ *
+ * blocks_updater: Keep the local database in sync with the latest block file data.
+ * This program should be kept running in the background.
+ *
+ */
+
+
+// TODO
